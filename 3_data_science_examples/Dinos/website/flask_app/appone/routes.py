ef6ea47489f2e90844c22490841ba9be6f0f7958
Amii Whelan <amii.whelan@heygo.com>
2022-08-07 18:23:08 +0100

    chore: working reccs

--- 3_data_science_examples/Dinos/website/flask_app/appone/routes.py
+++ 3_data_science_examples/Dinos/website/flask_app/appone/routes.py
@@ -1,8 +1,9 @@
 from appone.models import DinosaurDB, DinosaurSelections
-from appone import app
+from appone import app, db
 from flask import render_template, request, url_for, flash
 from appone.dinosaurList import dinosaurs
 from appone.recommender_code import get_recommendations
+from appone.models import DinosaurLiked, DinosaurSelections
 
 
 @app.route("/")
@@ -19,4 +20,16 @@ def dinosaur():
 def selectDinosaur():
     selectedDinosaur = request.form.get('dinosaur')
     result = get_recommendations(selectedDinosaur)
+    dino = DinosaurSelections(reccommendations=str(result))
+    db.session.add(dino)
+    db.session.commit()
     return render_template('dinosaur.html', selectedDinosaur=selectedDinosaur, result=result, dinosaurs=dinosaurs)
+
+
+@app.route('/likedDinosaur', methods=['POST'])
+def likedDinosaur():
+    likedDinosaur = request.form.get('dinosaur')
+    dino = DinosaurLiked(likedDinosaur=str(likedDinosaur))
+    db.session.add(dino)
+    db.session.commit()
+    return render_template('thankYou.html')
